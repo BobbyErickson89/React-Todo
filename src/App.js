d286f1dd4d486f1425fc7343d67f5912fb917e3a
Bobby Erickson <ericksonr07@gmail.com>
2018-04-14 22:31:51 -0400

    refactored directory structures and started todo_list component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import TodoList from './components/todo_list';
 
 class App extends Component {
   render() {
     return (
-        <div>My React App!</div>
+        <div>
+            My React App!
+            <TodoList />
+        </div>
     );
   }
 }
---
+++ src/components/todo_list.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>My list of todos</div>
+        )
+    }
+}
+
+export default TodoList;
