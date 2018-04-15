0986b05db74188ae873013d308919848eee2bcc0
Bobby Erickson <ericksonr07@gmail.com>
2018-04-14 21:06:51 -0400

    testing git on personal computer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-        <div>My React App</div>
+        <div>My React App!</div>
     );
   }
 }
